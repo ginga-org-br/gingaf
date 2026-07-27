1545ef306c94705d6d5abc46f86d9c296f614634
Alan Guedes <a.guedes@reading.ac.uk>
2026-07-27 15:18:25 +0100

    ncl: test user_profile_select from 1 and n

---
+++ ncldoc/test/user_profile_select_from_1_test.dart
@@ -0,0 +1,125 @@
+import 'package:ncldoc/ncl_document.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('NCL Single User Profile Tests', () {
+    test(
+      'verifies active user dynamic property update and instant switch constituent re-evaluation',
+      () {
+        final xml = '''
+<ncl id="dynPropDoc">
+<head>
+<ruleBase>
+  <rule id="rCC" var="closedCaptioning" comparator="eq" value="true"/>
+</ruleBase>
+</head>
+<body>
+<settings id="userSettings" type="application/x-ncl-user-settings" user="currentUser">
+  <property name="closedCaptioning"/>
+</settings>
+<port id="p1" component="swSub"/>
+<switch id="swSub">
+  <bindRule rule="rCC" constituent="mSub"/>
+  <defaultComponent component="mNoSub"/>
+  <media id="mSub" src="video_cc.mp4"/>
+  <media id="mNoSub" src="video.mp4"/>
+</switch>
+</body>
+</ncl>
+''';
+
+        final doc = NCLDocument.fromXML(xml);
+        final user = NCLUserData(
+          id: 'u1',
+          name: 'Viewer',
+          initialProperties: {'closedCaptioning': false},
+        );
+
+        doc.users.registerUser(user);
+        doc.users.setActiveUser('u1');
+
+        final sw = doc.getNodeById('swSub') as Switch;
+        expect(doc.evaluateRule('rCC'), isFalse);
+        expect(doc.resolveSwitch(sw)?.id, equals('mNoSub'));
+
+        doc.users.setUserProperty('u1', 'closedCaptioning', true);
+        expect(doc.evaluateRule('rCC'), isTrue);
+        expect(doc.resolveSwitch(sw)?.id, equals('mSub'));
+      },
+    );
+
+    test('verifies all required viewer profile basic attributes', () {
+      final xml = '''
+    <ncl>
+      <head>
+        <settings id="userSettings" type="application/x-ncl-user-settings" user="currentUser">
+          <property name="nickname"/>
+          <property name="parentalControl"/>
+          <property name="maxContentRating"/>
+          <property name="avatar"/>
+          <property name="audioLanguage"/>
+          <property name="closedCaptioningLanguage"/>
+          <property name="userInterfaceLanguage"/>
+          <property name="closedCaptioning"/>
+          <property name="closedSigning"/>
+          <property name="closedSigningSide"/>
+          <property name="closedSigningWidth"/>
+          <property name="audioDescription"/>
+          <property name="dialogEnhancement"/>
+          <property name="voiceGuidance"/>
+        </settings>
+      </head>
+      <body>
+        <media id="m1"/>
+      </body>
+    </ncl>
+    ''';
+      final doc = NCLDocument.fromXML(
+        xml,
+        usersDataJson: '''
+    {
+      "id": "uViewer1",
+      "name": "Alice",
+      "properties": {
+        "nickname": "Alice",
+        "parentalControl": true,
+        "maxContentRating": "14",
+        "avatar": "alice.png",
+        "audioLanguage": "pt-BR",
+        "closedCaptioningLanguage": "pt-BR",
+        "userInterfaceLanguage": "pt-BR",
+        "closedCaptioning": true,
+        "closedSigning": false,
+        "closedSigningSide": "right",
+        "closedSigningWidth": 28,
+        "audioDescription": false,
+        "dialogEnhancement": false,
+        "voiceGuidance": false
+      }
+    }
+    ''',
+      );
+
+      final user = doc.users.activeUser;
+      expect(user, isNotNull);
+      expect(user!.id, equals('uViewer1'));
+
+      final settingsNode = doc.getElementById('userSettings') as Settings;
+
+      expect(doc.getPropertyValue(settingsNode, 'nickname'), equals('Alice'));
+      expect(doc.getPropertyValue(settingsNode, 'parentalControl'), equals('true'));
+      expect(doc.getPropertyValue(settingsNode, 'maxContentRating'), equals('14'));
+      expect(doc.getPropertyValue(settingsNode, 'avatar'), equals('alice.png'));
+      expect(doc.getPropertyValue(settingsNode, 'audioLanguage'), equals('pt-BR'));
+      expect(doc.getPropertyValue(settingsNode, 'closedCaptioningLanguage'), equals('pt-BR'));
+      expect(doc.getPropertyValue(settingsNode, 'userInterfaceLanguage'), equals('pt-BR'));
+      expect(doc.getPropertyValue(settingsNode, 'closedCaptioning'), equals('true'));
+      expect(doc.getPropertyValue(settingsNode, 'closedSigning'), equals('false'));
+      expect(doc.getPropertyValue(settingsNode, 'closedSigningSide'), equals('right'));
+      expect(doc.getPropertyValue(settingsNode, 'closedSigningWidth'), equals('28'));
+      expect(doc.getPropertyValue(settingsNode, 'audioDescription'), equals('false'));
+      expect(doc.getPropertyValue(settingsNode, 'dialogEnhancement'), equals('false'));
+      expect(doc.getPropertyValue(settingsNode, 'voiceGuidance'), equals('false'));
+    });
+  });
+}
---
+++ ncldoc/test/user_profile_select_from_n_test.dart
@@ -0,0 +1,259 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:ncldoc/ncl_document.dart';
+import 'package:ncldoc/users.dart';
+import 'package:ncldoc/elements.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('NCL Multi User Profile Tests', () {
+
+test('verifies multi-user profile evaluation with composite rules', () {
+      final xml = '''
+<ncl id="multiUserDoc">
+<head>
+<ruleBase>
+  <rule id="rAdult" var="age" comparator="gt" value="16"/>
+  <rule id="rCC" var="closedCaptioning" comparator="eq" value="true"/>
+  <compositeRule id="rAdultWithCC" operator="and">
+    <rule id="rAdult"/>
+    <rule id="rCC"/>
+  </compositeRule>
+</ruleBase>
+<descriptorBase>
+  <descriptor id="d1"/>
+</descriptorBase>
+</head>
+<body>
+    <settings id="userSettings" type="application/x-ncl-user-settings" user="currentUser">
+  <property name="age"/>
+  <property name="closedCaptioning"/>
+</settings>
+<port id="pMain" component="swAd"/>
+<switch id="swAd">
+  <bindRule rule="rAdultWithCC" constituent="mAdultCCAd"/>
+  <bindRule rule="rAdult" constituent="mAdultAd"/>
+  <media id="mAdultCCAd" src="adult_cc.mp4" descriptor="d1"/>
+  <media id="mAdultAd" src="adult.mp4" descriptor="d1"/>
+  <media id="mGeneralAd" src="general.mp4" descriptor="d1"/>
+</switch>
+</body>
+</ncl>
+''';
+
+      final docAdultCC = NCLDocument.fromXML(
+        xml,
+        baseURI: Uri.file(Directory.systemTemp.path + '/'),
+        usersDataJson: '[{"id": "u1", "name": "AdultCC", "properties": {"age": "25", "closedCaptioning": "true"}}]',
+      );
+
+      final sw1 = docAdultCC.getNodeById('swAd') as Switch;
+      expect(docAdultCC.evaluateRule('rAdultWithCC'), isTrue);
+      expect(docAdultCC.resolveSwitch(sw1)?.id, equals('mAdultCCAd'));
+
+      final docAdultNoCC = NCLDocument.fromXML(
+        xml,
+        baseURI: Uri.file(Directory.systemTemp.path + '/'),
+        usersDataJson: '[{"id": "u2", "name": "AdultNoCC", "properties": {"age": "25", "closedCaptioning": "false"}}]',
+      );
+
+      final sw2 = docAdultNoCC.getNodeById('swAd') as Switch;
+      expect(docAdultNoCC.evaluateRule('rAdultWithCC'), isFalse);
+      expect(docAdultNoCC.evaluateRule('rAdult'), isTrue);
+      expect(docAdultNoCC.resolveSwitch(sw2)?.id, equals('mAdultAd'));
+    });
+
+test('verifies dynamic user property update and rule re-evaluation', () {
+      final xml = '''
+<ncl id="dynamicDoc">
+<head>
+<ruleBase>
+  <rule id="rAdult" var="age" comparator="gte" value="18"/>
+</ruleBase>
+</head>
+<body>
+    <settings id="userSettings" type="application/x-ncl-user-settings" user="currentUser">
+  <property name="age"/>
+</settings>
+<port id="pMain" component="swContent"/>
+<switch id="swContent">
+  <bindRule rule="rAdult" constituent="mRestricted"/>
+  <defaultComponent component="mPublic"/>
+  <media id="mRestricted" src="restricted.mp4"/>
+  <media id="mPublic" src="public.mp4"/>
+</switch>
+</body>
+</ncl>
+''';
+
+      final doc = NCLDocument.fromXML(
+        xml,
+        baseURI: Uri.file(Directory.systemTemp.path + '/'),
+        usersDataJson: '[{"id": "u1", "name": "YoungUser", "properties": {"age": "16"}}]',
+      );
+
+      final sw = doc.getNodeById('swContent') as Switch;
+      expect(doc.evaluateRule('rAdult'), isFalse);
+      expect(doc.resolveSwitch(sw)?.id, equals('mPublic'));
+
+      doc.users.setUserProperty('u1', 'age', '20');
+      expect(doc.evaluateRule('rAdult'), isTrue);
+      expect(doc.resolveSwitch(sw)?.id, equals('mRestricted'));
+    });
+
+test('verifies active user switching in document context and rule re-evaluation', () {
+      final xml = '''
+<ncl id="switchUserDoc">
+<head>
+<ruleBase>
+  <rule id="rPortuguese" var="lang" comparator="eq" value="pt-BR"/>
+</ruleBase>
+</head>
+<body>
+    <settings id="userSettings" type="application/x-ncl-user-settings" user="currentUser">
+  <property name="lang"/>
+</settings>
+<port id="pMain" component="swLang"/>
+<switch id="swLang">
+  <bindRule rule="rPortuguese" constituent="mPT"/>
+  <defaultComponent component="mEN"/>
+  <media id="mPT" src="audio_pt.mp3"/>
+  <media id="mEN" src="audio_en.mp3"/>
+</switch>
+</body>
+</ncl>
+''';
+
+      final doc = NCLDocument.fromXML(
+        xml,
+        baseURI: Uri.file(Directory.systemTemp.path + '/'),
+        usersDataJson: '[{"id": "u1", "name": "User PT", "properties": {"lang": "pt-BR"}}, {"id": "u2", "name": "User EN", "properties": {"lang": "en-US"}}]',
+      );
+
+      final sw = doc.getNodeById('swLang') as Switch;
+      expect(doc.users.activeUser?.id, equals('u1'));
+      expect(doc.evaluateRule('rPortuguese'), isTrue);
+      expect(doc.resolveSwitch(sw)?.id, equals('mPT'));
+
+      doc.users.setActiveUser('u2');
+      expect(doc.users.activeUser?.id, equals('u2'));
+      expect(doc.evaluateRule('rPortuguese'), isFalse);
+      expect(doc.resolveSwitch(sw)?.id, equals('mEN'));
+    });
+
+test('verifies multi-user session profile switching and dynamic rule evaluation', () {
+      final xml = '''
+<ncl id="sessProfileDoc">
+<head>
+<ruleBase>
+  <rule id="rAdult" var="age" comparator="gte" value="18"/>
+  <rule id="rMinor" var="age" comparator="lt" value="18"/>
+</ruleBase>
+</head>
+<body>
+    <settings id="userSettings" type="application/x-ncl-user-settings" user="currentUser">
+  <property name="age"/>
+  <property name="closedCaptioning"/>
+</settings>
+<port id="pMain" component="swAd"/>
+<switch id="swAd">
+  <bindRule rule="rAdult" constituent="mAdult"/>
+  <bindRule rule="rMinor" constituent="mMinor"/>
+  <media id="mAdult" src="ad_adult.mp4"/>
+  <media id="mMinor" src="ad_minor.mp4"/>
+</switch>
+</body>
+</ncl>
+''';
+
+      final doc = NCLDocument.fromXML(xml);
+      final u1 = NCLUserData(id: 'u1', name: 'Adult', initialProperties: {'age': 25});
+      final u2 = NCLUserData(id: 'u2', name: 'Child', initialProperties: {'age': 10});
+
+      doc.users.registerUser(u1);
+      doc.users.registerUser(u2);
+
+      doc.users.createUsersSession('family_room');
+      doc.users.joinUsersSession('family_room', 'u1');
+      doc.users.joinUsersSession('family_room', 'u2');
+
+      doc.users.setActiveUser('u1');
+      final sw = doc.getNodeById('swAd') as Switch;
+      expect(doc.evaluateRule('rAdult'), isTrue);
+      expect(doc.resolveSwitch(sw)?.id, equals('mAdult'));
+
+      doc.users.setActiveUser('u2');
+      expect(doc.evaluateRule('rMinor'), isTrue);
+      expect(doc.resolveSwitch(sw)?.id, equals('mMinor'));
+    });
+
+    test('verifies multi-user session profile export import, migration, and manager teardown', () {
+      final xml = '''
+<ncl id="sessionDoc">
+<head>
+<ruleBase>
+  <rule id="rAdult" var="age" comparator="gte" value="18"/>
+  <rule id="rLangEN" var="preferredLang" comparator="eq" value="en"/>
+  <compositeRule id="rAdultEN" operator="and">
+    <rule id="rAdult"/>
+    <rule id="rLangEN"/>
+  </compositeRule>
+</ruleBase>
+</head>
+<body>
+<settings id="userSettings" type="application/x-ncl-user-settings" user="currentUser">
+  <property name="age"/>
+  <property name="preferredLang"/>
+</settings>
+<port id="pMain" component="swAd"/>
+<switch id="swAd">
+  <bindRule rule="rAdultEN" constituent="mAdultEN"/>
+  <bindRule rule="rAdult" constituent="mAdult"/>
+  <defaultComponent component="mDefault"/>
+  <media id="mAdultEN" src="en_adult.mp4"/>
+  <media id="mAdult" src="adult.mp4"/>
+  <media id="mDefault" src="default.mp4"/>
+</switch>
+</body>
+</ncl>
+''';
+
+      final doc = NCLDocument.fromXML(xml);
+      final users = doc.users;
+      final u1 = NCLUserData(id: 'u1', name: 'Alice', initialProperties: {'age': 25, 'preferredLang': 'en'});
+      final u2 = NCLUserData(id: 'u2', name: 'Bob', initialProperties: {'age': 16, 'preferredLang': 'es'});
+
+      users.registerUser(u1);
+      users.registerUser(u2);
+      users.createUsersSession('living_room');
+      users.joinUsersSession('living_room', 'u1');
+      users.joinUsersSession('living_room', 'u2');
+
+      users.setActiveUser('u1');
+      final sw = doc.getNodeById('swAd') as Switch;
+      expect(doc.evaluateRule('rAdultEN'), isTrue);
+      expect(doc.resolveSwitch(sw)?.id, equals('mAdultEN'));
+
+      users.setUserProperty('u1', 'preferredLang', 'es');
+      expect(doc.evaluateRule('rAdultEN'), isFalse);
+      expect(doc.evaluateRule('rAdult'), isTrue);
+      expect(doc.resolveSwitch(sw)?.id, equals('mAdult'));
+
+      final jsonExport = jsonEncode(users.allUsers.map((u) => u.toJson()).toList());
+      expect(jsonExport.contains('Alice'), isTrue);
+      expect(jsonExport.contains('es'), isTrue);
+
+
+      final replicaUsers = NCLUsers();
+      replicaUsers.importUsers(jsonDecode(jsonExport) as List);
+      expect(replicaUsers.getUser('u1')?.name, equals('Alice'));
+
+
+      users.leaveUsersSession('living_room', 'u2');
+      users.removeUsersSession('living_room');
+      users.clear();
+      expect(users.getUser('u1'), isNull);
+    });
+  });
+}
+
